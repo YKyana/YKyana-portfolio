13b3ff6fb788c3d9fb9e4bb92f1c428770f59935
YKyana <69356865+YKyana@users.noreply.github.com>
2026-07-06 18:19:50 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
+const menuButton = document.querySelector(".menu-toggle");
+const menu = document.querySelector(".nav-menu");
 
+menuButton.addEventListener("click", () => {
+
+    menu.classList.toggle("active");
+
+});
