b25a8bea9b955eb20764427f8e919ab35ec89f22
YKyana <69356865+YKyana@users.noreply.github.com>
2026-07-07 13:17:36 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,13 +3,22 @@ const navMenu = document.querySelector(".nav-menu");
 const navLinks = document.querySelectorAll(".nav-menu a");
 
 menuToggle.addEventListener("click", () => {
+
     menuToggle.classList.toggle("active");
     navMenu.classList.toggle("active");
+
+    document.body.classList.toggle("menu-open");
+
 });
 
 navLinks.forEach(link => {
+
     link.addEventListener("click", () => {
+
         menuToggle.classList.remove("active");
         navMenu.classList.remove("active");
+        document.body.classList.remove("menu-open");
+
     });
+
 });
