19d5c635294259da5b5a01222cfe96df640958b7
YKyana <69356865+YKyana@users.noreply.github.com>
2026-07-08 09:46:56 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -16,3 +16,24 @@ navLinks.forEach(link => {
         document.body.classList.remove("menu-open");
     });
 });
+
+// Theme Toggle
+const themeToggle = document.getElementById('theme-toggle');
+const body = document.body;
+
+const currentTheme = localStorage.getItem('theme');
+if (currentTheme === 'light') {
+    body.classList.add('light');
+    themeToggle.classList.add('light');
+}
+
+themeToggle.addEventListener('click', () => {
+    body.classList.toggle('light');
+    themeToggle.classList.toggle('light');
+    
+    if (body.classList.contains('light')) {
+        localStorage.setItem('theme', 'light');
+    } else {
+        localStorage.setItem('theme', 'dark');
+    }
+});
